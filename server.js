01ff48c32755e5c0c67109d635b975cd1dad8bdd
DanielVZyl <daniel.vanzyl@bbd.co.za>
2025-01-20 14:05:16 +0200

    Hello Endpoint

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/hello', (req, res) => {
+    res.send('Hello ' + req.query.name)
 })
 
 app.listen(port, () => {
