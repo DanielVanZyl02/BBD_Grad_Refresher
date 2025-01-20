240b3009a463e3f22d11f2641859a49384f2e75f
DanielVanZyl02 <Daniel.VanZyl@bbd.co.za>
2025-01-20 15:08:06 +0200

    Update workflow

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
+require('dotenv').config();
+
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
